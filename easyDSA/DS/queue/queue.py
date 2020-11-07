b9b5c82d1b727d5a0d0755cd61f6841109debdf2
harsh gupta <harshnative@gmail.com>
2020-11-07 15:08:44 +0530

    added queue basic

---
+++ easyDSA/DS/queue/queue.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+
+# main class to implement queue
+class Queue:
+
+    def __init__(self):
+        self.queue = deque()
+    
+    # method to push data into queue
+    def enqueue(self , *data):
+        self.queue.append(list(data))
+
+    # method to get the first element of queue
+    def peek(self):
+        if(len(self.queue) > 0):
+            return self.queue[0]
+        else:
+            return None
+
+    # method to check whether the queue is empty or not
+    def isEmpty(self):
+        if(len(self.queue) > 0):
+            return False
+        else:
+            return True
+
+    # method to remove the first element from queue
+    def dequeue(self):
+        try:
+            return self.queue.popleft()
+        except IndexError:
+            return None
+    
+
+if __name__ == "__main__":
+    
+    q = Queue()
+
+    q.enqueue(1 , 2)
+    q.enqueue(2 , 3)
+    q.enqueue(3 , 4)
+    q.enqueue(4 , 5)
+    print(q.peek())
+    print(q.isEmpty())
+    print(q.dequeue())
+    print(q.peek())
+    print(q.isEmpty())
+    print(q.dequeue())
+    print(q.peek())
+    print(q.isEmpty())
+    print(q.dequeue())
+    print(q.peek())
+    print(q.isEmpty())
+    print(q.dequeue())
+    print(q.peek())
+    print(q.isEmpty())
+    print(q.dequeue())
+    print(q.peek())
+    print(q.isEmpty())
+    print(q.dequeue())
