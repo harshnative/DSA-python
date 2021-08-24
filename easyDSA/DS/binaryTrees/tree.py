546522dec282d0998b16f07ed183353ccca99707
harshnative <harshnative@gmail.com>
2021-08-24 22:13:56 +0530

    added deletion function

--- easyDSA/DS/binaryTrees/tree.py
+++ easyDSA/DS/binaryTrees/tree.py
@@ -1,10 +1,11 @@
 # class to init a new node
 class Node:
-    def __init__(self , data , left=None , right=None , parent=None):
+    def __init__(self , data , left=None , right=None , parent=None , frequency = 1):
         self.left = left
         self.right = right
         self.data = data
         self.parent = parent
+        self.frequency = frequency
 
 
 
@@ -38,10 +39,16 @@ class BinaryTree:
                     prevNode = currentNode
                     currentNode = currentNode.left
 
-                # else it needs to be insert on rigth
-                else:
+                # else it needs to be insert on right
+                elif(data > currentNode.data):
                     prevNode = currentNode
                     currentNode = currentNode.right
+
+                
+                # if the data is already in table , increase the frequency
+                else:
+                    currentNode.frequency = currentNode.frequency + 1
+                    return False
 
                 # if the currentNode becomes None , that is we want to insert the node here
                 if(currentNode == None):
@@ -56,6 +63,8 @@ class BinaryTree:
             else:
                 prevNode.right = newNode
 
+            return True
+
 
 
     # function to do the inoder traversal
@@ -68,7 +77,7 @@ class BinaryTree:
             nonlocal result
 
             # we need to print in this way
-            """left , root , rigth"""
+            """left , root , right"""
             if(currentNode != None):
                 traverse(currentNode.left)
                 
@@ -95,7 +104,7 @@ class BinaryTree:
             nonlocal result
 
             # we need to print in this way
-            """root , left , rigth"""
+            """root , left , right"""
             if(currentNode != None):
                 
                 result = result + str(currentNode.data) + seperator
@@ -122,7 +131,7 @@ class BinaryTree:
             nonlocal result
 
             # we need to print in this way
-            """left , rigth , root"""
+            """left , right , root"""
             if(currentNode != None):
 
                 traverse(currentNode.left)
@@ -139,6 +148,108 @@ class BinaryTree:
 
         return result
 
+
+    
+    # function to delete a node
+    def deleteNode_data(self , data , deleteAll = True , method = "inorder_predecessor"):
+
+        toDelete = None
+
+        # inner recursive function
+        def traverse(currentNode):
+
+            nonlocal toDelete
+
+            if(currentNode != None):
+                
+                if(currentNode.data == data):
+                    toDelete = currentNode
+                    return
+                else:
+                    traverse(currentNode.left)
+                    if(toDelete == None):
+                        traverse(currentNode.right)
+
+        traverse(currentNode = self.root)
+
+        if(toDelete == None):
+            return None
+
+        if((toDelete.left == None) and (toDelete.right == None)):
+            parent = toDelete.parent
+
+            if(parent.left == toDelete):
+                parent.left = None
+            else:
+                parent.right = None
+
+            del toDelete
+
+        elif((toDelete.left == None) or (toDelete.right == None)):
+            parent = toDelete.parent
+
+            if(parent.left == toDelete):
+                if(toDelete.left != None):
+                    parent.left = toDelete.left
+                else:
+                    parent.left = toDelete.right
+
+            else:
+                if(toDelete.left != None):
+                    parent.right = toDelete.left
+                else:
+                    parent.right = toDelete.right
+
+            del toDelete
+
+        else:
+
+            if(method == "inorder_predecessor"):
+                largest = None
+
+                currentNode = self.root.left
+
+                while(currentNode.right != None):
+
+                    largest = currentNode
+                    currentNode = currentNode.right
+
+                currentNodeData = currentNode.data
+
+                currentNodeParent = currentNode.parent
+
+                currentNodeParent.right = None
+
+                toDelete.data = currentNodeData
+
+                del currentNode
+
+            else:
+                smallest = None
+
+                currentNode = self.root.right
+
+                while(currentNode.left != None):
+
+                    largest = currentNode
+                    currentNode = currentNode.left
+
+                currentNodeData = currentNode.data
+
+                currentNodeParent = currentNode.parent
+
+                currentNodeParent.left = None
+
+                toDelete.data = currentNodeData
+
+                del currentNode
+
+
+
+
+
+
+
             
 
 
@@ -146,18 +257,33 @@ class BinaryTree:
 # for testing purpose only
 if __name__ == "__main__":
 
-    obj = BinaryTree(20)
+    obj = BinaryTree()
 
+    obj.insertIntoTree(11)
+    obj.insertIntoTree(6)
+    obj.insertIntoTree(4)
+    obj.insertIntoTree(5)
+    obj.insertIntoTree(9)
     obj.insertIntoTree(10)
-    obj.insertIntoTree(12)
-
+    obj.insertIntoTree(20)
+    obj.insertIntoTree(17)
+    obj.insertIntoTree(42)
     obj.insertIntoTree(30)
-    obj.insertIntoTree(25)
-    obj.insertIntoTree(22)
+    obj.insertIntoTree(50)
 
-    obj.insertIntoTree(40)
-    obj.insertIntoTree(35)
-    obj.insertIntoTree(45)
+
+
+
+
+
+
+    print(obj.inOrderTraversal())
+    print(obj.preOrderTraversal())
+    print(obj.postOrderTraversal())
+
+    obj.deleteNode_data(11 , method="inorder_successor")
+
+    print()
 
     print(obj.inOrderTraversal())
     print(obj.preOrderTraversal())
